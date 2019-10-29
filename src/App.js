dba566a4a0c26178f7ef500e20702b31c18627b5
xthian82 <crecalde@gmail.com>
2019-10-28 22:51:46 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hey Jerry segewg
         </a>
       </header>
     </div>
